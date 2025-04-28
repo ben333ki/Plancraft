86c6d80ff9829caf8f08807675c3c712ac9b6ebe
TheBest148 <bestza148@gmail.com>
2025-04-28 21:58:32 +0700

    Update-route-navbar/profile/editpf

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateAccount from './components/CreateAccount';
 import Login from './components/Login';
 import Home from './components/Home';
+import Profile from './components/Profile';
+import EditProfile from './components/Edit-profile';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/edit-profile" element={<EditProfile />} />
       </Routes>
     </Router>
   );
