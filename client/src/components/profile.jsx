f26383476e324d99e331d4f8881542783d8d65dd
ben333ki <kosinben@gmail.com>
2025-04-28 14:58:36 +0700

    update profile

---
+++ client/src/components/profile.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'; // 🆕 Import useNavigate
+import '../styles/login.css';
+import Leaves from './Leaves';
+
+const Profile = () => {
+  return (
+    <>
+        <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default Profile;
